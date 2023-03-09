504f95b321e2f875079af2d211f1b4dad9890902
Dominik Jones <dominikjo22@gmail.com>
2023-03-08 19:46:37 -0500

    js pratice

--- dom/red-yellow-green/script.js
+++ dom/red-yellow-green/script.js
@@ -27,8 +27,13 @@ greenDiv.onclick = () => console.log('green')
 
 const squares = document.querySelectorAll('.colorSquare')
 
-// forEach
-const timesClicked = {'red': 0, 'yellow': 0, 'green': 0}
+// forEach 
+const timesClicked = 
+{'red': 0, 
+'yellow': 0, 
+'green': 0
+}
+
 squares.forEach(square => {
   square.onclick = () => {
     timesClicked[square.value] += 1
